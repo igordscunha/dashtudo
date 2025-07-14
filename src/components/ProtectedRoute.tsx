3bf000d37b282bd1292065358595cd4c31dfab42
Igor Cunha <igordscunha@outlook.com>
2025-07-14 12:46:30 -0300

    refazendo o loading global

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { Loading } from './loading';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { isAuthenticated, loading } = useAuth();
+  const { isAuthenticated, loading } = useAuth()
 
   if (loading) {
-    return <div>Loading...</div>; // ou seu componente de loading
+    return <Loading/>
   }
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/login" replace />
   }
 
   return <>{children}</>;
---
+++ src/components/loading/index.tsx
@@ -0,0 +1,10 @@
+export const Loading = () => {
+  return(
+    <section>
+      <div className="loading-pulse p-6 md:p-4 flex flex-col justify-center items-center">
+        <img src='dashtudo1-nobg.png'/>
+        <h4 className="italic">Carregando... espere um momento...</h4>
+      </div>
+    </section>
+  )
+}
