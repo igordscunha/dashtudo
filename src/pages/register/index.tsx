fbdf77340538240e0e3d606b357a403944d13c5b
Igor Cunha <igordscunha@outlook.com>
2025-08-07 20:06:04 -0300

    mudando url

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -16,7 +16,7 @@ export const RegisterPage = () => {
     e.preventDefault();
 
     try{
-      await axios.post(`${baseUrl}users`, {
+      await axios.post(`${baseUrl}:3000/users`, {
         nome,
         sobrenome,
         data_nascimento,
